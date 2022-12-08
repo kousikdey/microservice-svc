aa897e50f3933cf32deb9098f04567e97973289f
kousik dey <kousik dey@LAPTOP-SGD84391>
2022-12-08 15:08:00 +0530

    packg fix in feature

--- src/main/java/com/in28minutes/microservices/limitsservice/bean/NewFeatureBean.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/bean/NewFeatureBean.java
@@ -1,7 +1,5 @@
-<<<<<<< Upstream, based on master
 package com.in28minutes.microservices.limitsservice.bean;
-=======
->>>>>>> a9b1326 Create NewFeatureBean.java
+
 public class NewFeatureBean{
   
   private int temp;
