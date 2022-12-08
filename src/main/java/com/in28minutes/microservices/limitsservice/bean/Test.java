c23b9677b0fd1bcea7115a0b4e81927bf045ad0b
kousik dey <kousik dey@LAPTOP-SGD84391>
2022-12-08 16:31:22 +0530

    master code add1

--- src/main/java/com/in28minutes/microservices/limitsservice/bean/Test.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/bean/Test.java
@@ -3,5 +3,15 @@ package com.in28minutes.microservices.limitsservice.bean;
 public class Test{
   
   private String abc;
+
+public String getAbc() {
+	return abc;
+}
+
+public void setAbc(String abc) {
+	this.abc = abc;
+}
+  
+  
   
 }
