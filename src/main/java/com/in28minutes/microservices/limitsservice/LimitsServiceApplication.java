3f47621e796096ea792d42cd13d8567082d38c17
kousik dey <kousik dey@LAPTOP-SGD84391>
2022-12-08 16:27:00 +0530

    rest client added

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,12 +2,19 @@ package com.in28minutes.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restClient() {
+		return new RestTemplate();
 	}
 
 }
